962f352f424255f09ed1c0d0618e9e60350546fb
AlexeyMarkevich <zviger.alexey@gmail.com>
2026-09-07 21:37:57 +0300

    Added entities

---
+++ backend/Products/Products.Domain/Product.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Entities;
+
+namespace Products.Domain
+{
+    public class Product : IEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string GeneralNote { get; set; }
+        public string SpecialNote { get; set; }
+    }
+}
