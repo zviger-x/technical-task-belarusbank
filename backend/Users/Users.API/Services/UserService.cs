ed7343be276d1740f5cb5ddcc90bd2e031dcba41
AlexeyMarkevich <zviger.alexey@gmail.com>
2026-09-07 19:46:09 +0300

    Added gRPC endpoint

---
+++ backend/Users/Users.API/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using MediatR;
+using Shared.Grpc.User;
+using Users.Application.UseCases.Queries;
+using GrpcUserService = Shared.Grpc.User.UserService;
+
+namespace Users.API.Services
+{
+    public class UserService : GrpcUserService.UserServiceBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override async Task<UserBlockStatusResponse> GetUserBlockStatus(UserBlockStatusRequest request, ServerCallContext context)
+        {
+            var query = new UserGetBlockStatusQuery(Guid.Parse(request.UserId));
+
+            var response = await _mediator.Send(query, context.CancellationToken);
+
+            if (!response.IsSuccess)
+                throw new RpcException(new Status(StatusCode.Internal, "An unexpected error occurred."));
+
+            return new UserBlockStatusResponse { IsBlocked = response.Data };
+        }
+    }
+}
---
+++ backend/Users/Users.Application/UseCases/Handlers/UserGetBlockStatusHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Shared.Common.Results;
+using Users.Application.Common.Errors;
+using Users.Application.UnitOfWork;
+using Users.Application.UseCases.Queries;
+
+namespace Users.Application.UseCases.Handlers
+{
+    public class UserGetBlockStatusHandler : BaseHandler, IRequestHandler<UserGetBlockStatusQuery, Result<bool>>
+    {
+        public UserGetBlockStatusHandler(IUnitOfWork unitOfWork, IMapper mapper)
+            : base(unitOfWork, mapper)
+        {
+        }
+
+        public async Task<Result<bool>> Handle(UserGetBlockStatusQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _unitOfWork.UserRepository.GetByIdAsync(request.Guid, cancellationToken);
+
+            if (user == null)
+                return Result.Failure<bool>(UserErrors.UserNotFound);
+
+            return Result.Success(user.IsBlocked);
+        }
+    }
+}
